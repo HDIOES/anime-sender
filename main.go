eeeb50feee810dc2106f07337137753307113be8
Ivan Timofeev <kiberaction@yandex.ru>
2020-01-18 22:23:55 +0300

    add sendMessage functionality

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/nats-io/nats.go"
+	"go.uber.org/dig"
+)
+
+func main() {
+	container := dig.New()
+	container.Provide(func() *Settings {
+		if jsonFile, openErr := os.Open("settings.json"); openErr != nil {
+			panic(openErr)
+		} else {
+			defer jsonFile.Close()
+			decoder := json.NewDecoder(jsonFile)
+			settings := &Settings{}
+			if decodeErr := decoder.Decode(settings); decodeErr != nil {
+				panic(decodeErr)
+			} else {
+				return settings
+			}
+		}
+	})
+	container.Provide(func(settings *Settings) (*nats.Conn, *TelegramService) {
+		natsConnection, ncErr := nats.Connect(settings.NatsURL)
+		if ncErr != nil {
+			panic(ncErr)
+		}
+		service := &TelegramService{
+			Client:   &http.Client{},
+			Settings: settings,
+		}
+		return natsConnection, service
+	})
+	container.Invoke(func(settings *Settings, telegramService *TelegramService, natsConnection *nats.Conn) {
+		natsConnection.Subscribe(settings.NatsSubject, telegramService.receiveNotification)
+		srv := &http.Server{Addr: ":8001"}
+		log.Fatal(srv.ListenAndServe())
+	})
+}
+
+//Settings mapping object for settings.json
+type Settings struct {
+	NatsURL       string `json:"natsUrl"`
+	NatsSubject   string `json:"natsSubject"`
+	TelegramToken string `json:"telegramToken"`
+	TelegramURL   string `json:"telegramUrl"`
+}
---
+++ service.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/HDIOES/anime-app/dao"
+	"github.com/nats-io/nats.go"
+)
+
+//TelegramService struct
+type TelegramService struct {
+	Client   *http.Client
+	Settings *Settings
+}
+
+func (ts *TelegramService) receiveNotification(msg *nats.Msg) {
+	if notification, notificationErr := decodeNotification(msg); notificationErr != nil {
+		switch notification.Type {
+		case "startCommand":
+			{
+				if err := ts.sendStartMessage(notification); err != nil {
+					fmt.Println(err)
+				}
+			}
+		case "animesCommand":
+			{
+				if err := ts.sendAnimesMessage(notification); err != nil {
+					fmt.Println(err)
+				}
+			}
+		case "subscriptionsCommand":
+			{
+				if err := ts.sendSubscriptionsMessage(notification); err != nil {
+					fmt.Println(err)
+				}
+			}
+		case "defaultCommand":
+			{
+				if err := ts.sendDefaultMessage(notification); err != nil {
+					fmt.Println(err)
+				}
+			}
+		}
+	}
+}
+
+func decodeNotification(msg *nats.Msg) (*Notification, error) {
+	notification := &Notification{}
+	unmarshalErr := json.Unmarshal(msg.Data, notification)
+	if unmarshalErr != nil {
+		return nil, unmarshalErr
+	}
+	return notification, nil
+}
+
+//Notification struct
+type Notification struct {
+	TelegramID int64          `json:"telegramId"`
+	Type       string         `json:"type"`
+	Text       string         `json:"text"`
+	Animes     []dao.AnimeDTO `json:"animes"`
+	WebhookURL string         `json:"webhookUrl"`
+}
+
+func (ts *TelegramService) sendStartMessage(notification *Notification) error {
+	data := url.Values{}
+	data.Set("text", notification.Text)
+	data.Set("chat_id", strconv.FormatInt(notification.TelegramID, 10))
+	_, resErr := ts.Client.Post(ts.Settings.TelegramURL+ts.Settings.TelegramToken+"/sendMessage", "application/x-www-form-urlencoded", strings.NewReader(data.Encode()))
+	if resErr != nil {
+		return resErr
+	}
+	return nil
+}
+
+func (ts *TelegramService) sendAnimesMessage(notification *Notification) error {
+	sendMessage := SendMessage{
+		ChatID:      notification.TelegramID,
+		Text:        notification.Text,
+		ReplyMarkup: InlineKeyboardMarkup{},
+	}
+	count := len(notification.Animes)
+	sendMessage.ReplyMarkup.InlineKeyboard = make([][]InlineKeyboardButton, count)
+	for i := 0; i < count; i++ {
+		sendMessage.ReplyMarkup.InlineKeyboard[i] = make([]InlineKeyboardButton, 1)
+		sendMessage.ReplyMarkup.InlineKeyboard[i][0] = InlineKeyboardButton{
+			Text: notification.Animes[i].EngName,
+		}
+	}
+	data, err := json.Marshal(sendMessage)
+	if err != nil {
+		return err
+	}
+	_, resErr := ts.Client.Post(ts.Settings.TelegramURL+ts.Settings.TelegramToken+"/sendMessage", "application/json", bytes.NewReader(data))
+	if resErr != nil {
+		return resErr
+	}
+	return nil
+}
+
+func (ts *TelegramService) sendSubscriptionsMessage(notification *Notification) error {
+	sendMessage := SendMessage{
+		ChatID:      notification.TelegramID,
+		Text:        notification.Text,
+		ReplyMarkup: InlineKeyboardMarkup{},
+	}
+	count := len(notification.Animes)
+	sendMessage.ReplyMarkup.InlineKeyboard = make([][]InlineKeyboardButton, count)
+	for i := 0; i < count; i++ {
+		sendMessage.ReplyMarkup.InlineKeyboard[i] = make([]InlineKeyboardButton, 1)
+		sendMessage.ReplyMarkup.InlineKeyboard[i][0] = InlineKeyboardButton{
+			Text: notification.Animes[i].EngName,
+		}
+	}
+	data, err := json.Marshal(sendMessage)
+	if err != nil {
+		return err
+	}
+	_, resErr := ts.Client.Post(ts.Settings.TelegramURL+ts.Settings.TelegramToken+"/sendMessage", "application/json", bytes.NewReader(data))
+	if resErr != nil {
+		return resErr
+	}
+	return nil
+}
+
+func (ts *TelegramService) sendDefaultMessage(notification *Notification) error {
+	data := url.Values{}
+	data.Set("text", notification.Text)
+	data.Set("chat_id", strconv.FormatInt(notification.TelegramID, 10))
+	_, resErr := ts.Client.Post(ts.Settings.TelegramURL+ts.Settings.TelegramToken+"/sendMessage", "application/x-www-form-urlencoded", strings.NewReader(data.Encode()))
+	if resErr != nil {
+		return resErr
+	}
+	return nil
+}
+
+//SendMessage struct
+type SendMessage struct {
+	ChatID      int64                `json:"chat_id"`
+	Text        string               `json:"text"`
+	ReplyMarkup InlineKeyboardMarkup `json:"reply_markup"`
+}
+
+//InlineKeyboardMarkup struct
+type InlineKeyboardMarkup struct {
+	InlineKeyboard [][]InlineKeyboardButton `json:"inline_keyboard"`
+}
+
+//InlineKeyboardButton struct
+type InlineKeyboardButton struct {
+	Text string `json:"text"`
+}
